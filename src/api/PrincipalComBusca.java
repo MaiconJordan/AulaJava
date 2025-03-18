be08e96ee07d2f4b081f09426a2de68defbccd1c
Maicon <maiconplus@gmail.com>
2025-03-17 22:01:00 -0300

    Busca dinamica

--- src/api/PrincipalComBusca.java
+++ src/api/PrincipalComBusca.java
@@ -5,19 +5,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
     public static void main(String[] args)  throws IOException, InterruptedException  {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome do filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String chave = "3826f258";
+        String endereco = "https://www.omdbapi.com/?t=" +busca + "&apikey=" + chave;
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=3826f258"))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                System.out.println(response.body());
-
+        System.out.println(response.body());
     }
 
        
