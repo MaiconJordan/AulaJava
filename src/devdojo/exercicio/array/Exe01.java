94b8cf10d766376b310666a35ee3fdbc60dc1264
Maicon <maiconplus@gmail.com>
2025-07-01 11:51:40 -0300

    exec array low

---
+++ src/devdojo/exercicio/array/Exe01.java
@@ -0,0 +1,17 @@
+package devdojo.exercicio.array;
+
+public class Exe01 {
+    public static void main(String[] args) {
+        
+
+        String[] nomesAmigos = {"Guy", "Lee", "Sasuke", "Itachi", "Naruto"};
+
+        for (String nomes : nomesAmigos) {
+            System.out.println(nomes);
+        }
+
+
+
+    }
+
+}
