187d4774b0cb4903b01e84cb7cc07b2856c1b6a8
Maicon <maiconplus@gmail.com>
2025-07-03 20:17:27 -0300

    metodo com retorno

---
+++ src/devdojo/metodos/MetodoDivisao.java
@@ -0,0 +1,15 @@
+package devdojo.metodos;
+
+public class MetodoDivisao {
+
+    public double divisao (int num , int num2){
+        if(num == 0 || num2 == 0){
+            return 0;
+            
+        }
+
+        return num / num2;
+
+    }
+
+}
---
+++ src/devdojo/metodos/teste/DivisaoTeste.java
@@ -0,0 +1,16 @@
+package devdojo.metodos.teste;
+
+import devdojo.metodos.MetodoDivisao;
+
+public class DivisaoTeste {
+
+    public static void main(String[] args) {
+        
+        MetodoDivisao divisao = new MetodoDivisao();
+
+        double r = divisao.divisao(10, 0);
+        System.out.println(r);
+
+    }
+
+}
