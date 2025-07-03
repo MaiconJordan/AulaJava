d11f4b040b266d066de1679018cbe7c7cccd6c25
Maicon <maiconplus@gmail.com>
2025-07-02 23:06:02 -0300

    Classe

---
+++ src/devdojo/exercicio/oo/Pessoa.java
@@ -0,0 +1,15 @@
+package devdojo.exercicio.oo;
+
+public class Pessoa {
+
+    int idade;
+    String nome;
+    String sobrenome;
+    String cpf;
+    String rg;
+    String dataNascimento;
+    String sexo;
+    String estadoCivil;
+    
+
+}
