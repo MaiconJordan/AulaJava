4754c10330a80c83fc9fc86e9b7ed520ae90b3a5
Maicon <maiconplus@gmail.com>
2025-07-03 19:44:46 -0300

    metodos

---
+++ src/devdojo/metodos/MetodoSomar.java
@@ -0,0 +1,9 @@
+package devdojo.metodos;
+
+public class MetodoSomar {
+
+    public void somar(){
+        System.out.println( 1 + 1);
+    }
+
+}
---
+++ src/devdojo/metodos/teste/SomarTeste.java
@@ -0,0 +1,13 @@
+package devdojo.metodos.teste;
+
+import devdojo.metodos.MetodoSomar;
+
+public class SomarTeste {
+    public static void main(String[] args) {
+        
+        MetodoSomar somar = new MetodoSomar();
+
+        somar.somar();
+    }
+
+}
