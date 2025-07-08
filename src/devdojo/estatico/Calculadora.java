ef9224de2379856c0f46cde301254204d8db3f7c
Maicon <maiconplus@gmail.com>
2025-07-08 09:16:12 -0300

    metodo static

---
+++ src/devdojo/estatico/Calculadora.java
@@ -0,0 +1,10 @@
+package devdojo.estatico;
+
+public class Calculadora {
+
+
+    public static void somar(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+
+}
---
+++ src/devdojo/estatico/CalculadoraTeste.java
@@ -0,0 +1,15 @@
+package devdojo.estatico;
+
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        
+        
+
+        
+
+        Calculadora.somar(5, 5);
+
+
+    }
+
+}
