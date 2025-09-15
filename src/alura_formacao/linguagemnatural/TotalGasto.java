241de06c2e7e77ff67ad55f9400c15e47ec9db78
Maicon <maiconplus@gmail.com>
2025-09-15 09:04:13 -0300

    algoritimo de gastos

---
+++ src/alura_formacao/linguagemnatural/TotalGasto.java
@@ -0,0 +1,35 @@
+package alura_formacao.linguagemnatural;
+
+import java.util.Scanner;
+
+public class TotalGasto {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int gastos = 0; 
+        double vl =1;       
+
+        while (vl != 0 ){
+            System.out.println("Informe seus gastos");
+            vl = sc.nextDouble();
+            
+
+            gastos += vl;
+        }
+
+        System.out.println(gastos);
+        
+
+
+
+        sc.close();
+
+        
+
+
+
+    }
+
+}
